7acfc409d7f68d4b4bac8a5316a367a52651147e
OtavioVB <otaviovb.developer@gmail.com>
2022-11-20 10:46:57 -0300

    base(archives): `Initial Commit`

---
+++ OVB.Domain.CrossCutting.DesignPatterns.Abstractions/Models/DTOs/DataTransferObjectBase.cs
@@ -0,0 +1,19 @@
+﻿namespace OVB.Domain.CrossCutting.DesignPatterns.Abstractions.Models.DTOs;
+
+public abstract class DataTransferObjectBase
+{
+    public Guid Identifier { get; set; }
+    public DateTime RegisteredOn { get; set; }
+    public DateTime LastModificationOn { get; set; }
+    public Guid RegisteredBy { get; set; }
+    public Guid LastModificationBy { get; set; }
+
+    protected DataTransferObjectBase(Guid identifier, DateTime registeredOn, DateTime lastModificationOn, Guid registeredBy, Guid lastModificationBy)
+    {
+        Identifier = identifier;
+        RegisteredOn = registeredOn;
+        LastModificationOn = lastModificationOn;
+        RegisteredBy = registeredBy;
+        LastModificationBy = lastModificationBy;
+    }
+}
